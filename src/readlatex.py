a13b94585aa1a71e0886af2db941be2064ce8f23
Kavi Gupta <kavi@globalmbox.com>
2016-03-25 17:44:20 -0700

    added basic script to enable migration to package format rather than modifying each thing individually

---
+++ src/readlatex.py
@@ -0,0 +1,23 @@
+
+from os import makedirs, chdir, system
+from os.path import exists, basename, dirname, abspath
+from shutil import copyfile
+from sys import argv
+
+script_location = abspath(argv[0])
+
+def readlatex(path):
+    print("Compiling %s" % path)
+    chdir(dirname(dirname(script_location)))
+    create_if_not_exists('out')
+    copyfile('src/readlatex.sty', 'out/readlatex.sty')
+    copyfile(path, 'out/' + basename(path))
+    chdir('out')
+    system('pdflatex ' + basename(path))
+    
+
+def create_if_not_exists(directory):
+    if not exists(directory):
+        makedirs(directory)
+
+readlatex(argv[1])
