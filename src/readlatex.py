96f71af8164ce02e44f6845670ae60969268e393
Kavi Gupta <kavi@globalmbox.com>
2016-03-26 00:32:26 -0700

    changed readlatex.py to go into the directory rather than the other way around

--- src/readlatex.py
+++ src/readlatex.py
@@ -1,23 +1,21 @@
 
-from os import makedirs, chdir, system
-from os.path import exists, basename, dirname, abspath
-from shutil import copyfile, rmtree
+from os import makedirs, chdir, system, remove
+from os.path import exists, basename, dirname, abspath, join
+from shutil import copyfile
 from sys import argv
 
 script_location = abspath(argv[0])
 
 def readlatex(path):
+    chdir(dirname(path))
     print("Compiling %s" % path)
-    rmtree('out')
-    chdir(dirname(dirname(script_location)))
     create_if_not_exists('out')
-    copyfile('src/readlatex.sty', 'out/readlatex.sty')
-    copyfile(path, 'out/' + basename(path))
-    chdir('out')
+    location = join(dirname(script_location), 'readlatex.sty')
+    copyfile(location, 'readlatex.sty')
     # make sure pdf output has correct references
     for _ in range(3):
         system('pdflatex ' + basename(path))
-    
+    remove('readlatex.sty')
 
 def create_if_not_exists(directory):
     if not exists(directory):
