8c529e8d29cdd417cf825d69bee392df36f66ed2
Kavi Gupta <kavi@globalmbox.com>
2016-03-25 17:47:29 -0700

    added thing that deletes out first and then compiles three times

--- src/readlatex.py
+++ src/readlatex.py
@@ -1,19 +1,22 @@
 
 from os import makedirs, chdir, system
 from os.path import exists, basename, dirname, abspath
-from shutil import copyfile
+from shutil import copyfile, rmtree
 from sys import argv
 
 script_location = abspath(argv[0])
 
 def readlatex(path):
     print("Compiling %s" % path)
+    rmtree('out')
     chdir(dirname(dirname(script_location)))
     create_if_not_exists('out')
     copyfile('src/readlatex.sty', 'out/readlatex.sty')
     copyfile(path, 'out/' + basename(path))
     chdir('out')
-    system('pdflatex ' + basename(path))
+    # make sure pdf output has correct references
+    for _ in range(3):
+        system('pdflatex ' + basename(path))
     
 
 def create_if_not_exists(directory):
